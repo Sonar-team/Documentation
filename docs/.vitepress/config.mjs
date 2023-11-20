913f899d3c67542e27e8dccdf105166800bc708c
akmot9 <avicocyprien@yahoo.com>
2023-11-20 14:59:50 +0100

    deploy fix3

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/Documentation',
+  base: '/Documentation/',
   title: "Documentation",
   description: "Documentaion du projet",
   themeConfig: {
